0868267a250ad2518d7273b226aef8c69d6145dd
Nick Lam <lamnicholas237@gmail.com>
2022-09-05 01:40:43 +1000

    refactor

---
+++ BLANK/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  BLANK
+//
+//  Created by Nick Lam on 4/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectedIndex = 0
+    
+    let icons = [
+        "house",
+        "arrow.left.arrow.right",
+        "calendar",
+        "person.3",
+        "checklist"
+    ]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        VStack() {
+                            Text("First Screen")
+                        }
+                        .navigationTitle("First Screen")
+                    }
+                default:
+                    NavigationView {
+                        VStack {
+                            Text("Other")
+                        }
+                        .navigationTitle("Other Screen")
+                    }
+                }
+            }
+            
+            Divider()
+            HStack {
+                ForEach(0..<5, id: \.self) { number in
+                    Spacer()
+                    Button(action: {
+                        self.selectedIndex = number
+                    }, label: {
+                        Image(systemName: icons[number])
+                            .font(.system(size: 25,
+                                          weight: .regular,
+                                          design: .default))
+                            .foregroundColor(selectedIndex == number ? Color(.label) : Color(UIColor.white))
+                            .frame(width: 30, height: 30)
+                    })
+                    Spacer()
+                }
+            }
+            .frame(height: 60)
+            .background(Color.gray)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
