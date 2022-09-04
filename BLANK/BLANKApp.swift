1c08e83342b7c72f90f2ac35dcc838068fb137e7
Nick Lam <lamnicholas237@gmail.com>
2022-09-04 22:25:42 +1000

    Initial Commit

---
+++ BLANK/BLANKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLANKApp.swift
+//  BLANK
+//
+//  Created by Nick Lam on 4/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct BLANKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
