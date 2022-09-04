39e9706aff96bab33fecbf242ca06c44aa08d11f
Nick Lam <lamnicholas237@gmail.com>
2022-09-04 22:55:11 +1000

    Created bottom tab bar

--- BLANK/ContentView.swift
+++ BLANK/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedIndex = 0
+    
+    let icons = [
+        "house",
+        "arrow.left.arrow.right",
+        "calendar",
+        "person.3",
+        "checklist"
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        VStack {
+                            Text("First Screen")
+                        }
+                        .navigationTitle("First Screen")
+                    }
+                default:
+                    NavigationView {
+                        VStack {
+                            Text("Other")
+                        }
+                        .navigationTitle("Other Screen")
+                    }
+                }
+            }
+            
+            Divider().frame(height: 40)
+            HStack {
+                ForEach(0..<5, id: \.self) { number in
+                    Spacer()
+                    Button(action: {
+                        self.selectedIndex = number
+                    }, label: {
+                        Image(systemName: icons[number])
+                            .font(.system(size: 25,
+                                          weight: .regular,
+                                          design: .default))
+                            .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                            .frame(width: 30, height: 40)
+                    })
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
