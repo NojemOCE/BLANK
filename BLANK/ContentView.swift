0d05701cbfdd7714c7b5785a54012e3be5ee3644
Nick Lam <lamnicholas237@gmail.com>
2022-09-12 00:27:53 +1000

    combine old and new

--- BLANK/ContentView.swift
+++ BLANK/ContentView.swift
@@ -2,14 +2,55 @@
 //  ContentView.swift
 //  BLANK
 //
-//  Created by Nick Lam on 11/9/2022.
+//  Created by Nick Lam on 4/9/2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedIndex = 0
+    
+    let icons = [
+        "house",
+        "arrow.left.arrow.right",
+        "calendar",
+        "person.3",
+        "checklist"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            ZStack {
+                switch selectedIndex {
+                case 1:
+                    HistoryView()
+                case 2:
+                    CalendarView()
+                case 3:
+                    FriendView()
+                case 4:
+                    TasksView()
+                default:
+                    HomeView()
+                }
+            }
+            
+            HStack {
+                ForEach(0..<icons.count, id: \.self) { number in
+                    Spacer()
+                    Button(action: {
+                        self.selectedIndex = number
+                    }, label: {
+                        Image(systemName: icons[number])
+                            .font(.system(size: 25))
+                            .foregroundColor(selectedIndex == number ? Color(.label) : Color(UIColor.lightGray))
+                            .frame(width: 30, height: 30)
+                    })
+                    Spacer()
+                }
+            }
+            .padding(.top, 10)
+        }
     }
 }
 
